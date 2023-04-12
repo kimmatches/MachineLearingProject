c3e5aec047a988aa967f0587cd526a28cf6e3ad0
user <peach3139@naver.com>
2023-04-12 17:31:05 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 데이터 불러오기
+data = pd.read_csv("train.csv")
+
+# 상관 관계 분석을 위한 heatmap 출력
+sns.heatmap(data.corr(), annot=True, cmap='coolwarm', linewidths=0.2)
+plt.show()
