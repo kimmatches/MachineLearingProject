1399060e30d669d9153a40dd56ac0bc516654f31
user <peach3139@naver.com>
2023-04-12 23:07:00 +0900

    다중회귀 (모델 정확도까지)

--- multiple regression.py
+++ multiple regression.py
@@ -2,6 +2,7 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
@@ -15,23 +16,42 @@ test_data = pd.read_csv("test.csv")
 # 상관 관계 분석을 위한 heatmap 출력
 sns.heatmap(train_data.corr(), annot=True, cmap='coolwarm', linewidths=0.2)
 plt.show()
-sns.heatmap(test_data.corr(), annot=True, cmap='coolwarm', linewidths=0.2)
-plt.show()
+
 # 상관 관계 분석 정보 -> 운동시간, 온도, BPM, 칼로리 소모량 / 키, 몸무게
 
-# # 변수 선택
-# X = data[["Exercise_Duration", "BPM", "Weight(lb)"]] # 독립 변수
-# y = data["Calories_Burned"] # 종속 변수
-#
+# 변수 선택
+input = train_data[["Exercise_Duration", "Body_Temperature(F)", "BPM"]].to_numpy() # 독립 변수
+target = train_data["Calories_Burned"].to_numpy() # 종속 변수
+X_test = test_data[["Exercise_Duration", "Body_Temperature(F)", "BPM"]].to_numpy()
+# plt.plot(input[:, 0]* 10000)
+# plt.plot(input[:, 1])
+# plt.plot(input[:, 2]* 10000)
+# plt.plot(target)
+# plt.show()
+
 # # 데이터 전처리
 # X = X.fillna(X.mean()) # 결측치 처리
 # scaler = StandardScaler()
 # X = scaler.fit_transform(X) # 스케일링
-#
+
+poly = PolynomialFeatures(degree=7, include_bias=False)
+poly.fit(input)
+train_poly = poly.transform(input)
+print(np.shape(train_poly))
+
+
+tr_in, ts_in, tr_out, ts_out = train_test_split(
+    train_poly, target, test_size=0.2, random_state=42)
 # # 데이터 분할
 # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-#
-# # 모델 학습
+
+# 정확도
+lr=LinearRegression()
+lr.fit(tr_in, tr_out)
+print(lr.score(tr_in, tr_out))
+print(lr.score(ts_in, ts_out))
+
+
 # model = LinearRegression()
 # model.fit(X_train, y_train)
 #
